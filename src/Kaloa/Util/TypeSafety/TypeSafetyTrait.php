02523ffe179354341bb4b80f8a03df5f55c6b281
Marc Ermshaus <marc@ermshaus.org>
2014-02-21 17:52:32 +0100

    TypeSafety: Change Exception to IllegalArgumentException. Remove ensure<Type> methods

--- src/Kaloa/Util/TypeSafety/TypeSafetyTrait.php
+++ src/Kaloa/Util/TypeSafety/TypeSafetyTrait.php
@@ -2,68 +2,66 @@
 
 namespace Kaloa\Util\TypeSafety;
 
-use Exception;
+use IllegalArgumentException;
 
 trait TypeSafetyTrait
 {
-    private function ensureBool()
-    {
-        foreach (func_get_args() as $param)
-            if (!is_bool($param))
-                throw new Exception('bool expected');
-    }
-
-    private function ensureFloat()
-    {
-        foreach (func_get_args() as $param)
-            if (!is_float($param))
-                throw new Exception('float expected');
-    }
-
-    private function ensureInt()
-    {
-        foreach (func_get_args() as $param)
-            if (!is_int($param))
-                throw new Exception('int expected');
-    }
-
-    private function ensureResource()
-    {
-        foreach (func_get_args() as $param)
-            if (!is_resource($param))
-                throw new Exception('resource expected');
-    }
-
-    private function ensureString()
-    {
-        foreach (func_get_args() as $param)
-            if (!is_string($param))
-                throw new Exception('string expected');
-    }
-
     /**
-     * @api
+     * @param string $types
+     * @param array $args
      */
     private function ensure($types, array $args)
     {
-        $this->ensureString($types);
+        if (!is_string($types)) {
+            throw new IllegalArgumentException('string expected');
+        }
 
         $cnt = count($args);
 
-        if (strlen($types) !== $cnt)
-            throw new Exception('type count does not match argument count');
+        if (strlen($types) !== $cnt) {
+            throw new IllegalArgumentException('type count does not match argument count');
+        }
 
-        $tmp = str_split($types);
-
-        for ($i = 0; $i < $cnt; $i++)
+        for ($i = 0; $i < $cnt; $i++) {
             switch ($types[$i]) {
-                case 'b': $this->ensureBool($args[$i]); break;
-                case 'f': $this->ensureFloat($args[$i]); break;
-                case 'i': $this->ensureInt($args[$i]); break;
-                case 'r': $this->ensureResource($args[$i]); break;
-                case 's': $this->ensureString($args[$i]); break;
-                case '-': /* skip */ break;
-                default: throw new Exception('unknown type at position ' . $i);
+                case 'b':
+                    if (!is_bool($args[$i])) {
+                        throw new IllegalArgumentException('bool expected');
+                    }
+                    break;
+
+                case 'f':
+                    if (!is_float($args[$i])) {
+                        throw new IllegalArgumentException('float expected');
+                    }
+                    break;
+
+                case 'i':
+                    if (!is_int($args[$i])) {
+                        throw new IllegalArgumentException('int expected');
+                    }
+                    break;
+
+                case 'r':
+                    if (!is_resource($args[$i])) {
+                        throw new IllegalArgumentException('resource expected');
+                    }
+                    break;
+
+                case 's':
+                    if (!is_string($args[$i])) {
+                        throw new IllegalArgumentException('string expected');
+                    }
+                    break;
+
+                case '-':
+                    /* skip */
+                    break;
+
+                default:
+                    throw new IllegalArgumentException('Unknown type at offset ' . $i . '. One of [bfirs-] expected');
+                    break;
             }
+        }
     }
 }
