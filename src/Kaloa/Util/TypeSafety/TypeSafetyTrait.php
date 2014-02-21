523c5e633f94a1d0f889798ee4fde4b07c233f05
Marc Ermshaus <marc@ermshaus.org>
2014-02-21 18:28:32 +0100

    TypeSafety: Support non-zero-indexed arrays in ensure(). Improve phrasing of Exception messages

--- src/Kaloa/Util/TypeSafety/TypeSafetyTrait.php
+++ src/Kaloa/Util/TypeSafety/TypeSafetyTrait.php
@@ -13,43 +13,45 @@ trait TypeSafetyTrait
     private function ensure($types, array $args)
     {
         if (!is_string($types)) {
-            throw new IllegalArgumentException('string expected');
+            throw new IllegalArgumentException('Type list must be of type string');
         }
 
         $cnt = count($args);
 
         if (strlen($types) !== $cnt) {
-            throw new IllegalArgumentException('type count does not match argument count');
+            throw new IllegalArgumentException('Type list length does not match argument count');
         }
 
-        for ($i = 0; $i < $cnt; $i++) {
+        $i = 0;
+
+        foreach ($args as $arg) {
             switch ($types[$i]) {
                 case 'b':
-                    if (!is_bool($args[$i])) {
+                    if (!is_bool($arg)) {
                         throw new IllegalArgumentException('bool expected');
                     }
                     break;
 
                 case 'f':
-                    if (!is_float($args[$i])) {
+                    if (!is_float($arg)) {
                         throw new IllegalArgumentException('float expected');
                     }
                     break;
 
                 case 'i':
-                    if (!is_int($args[$i])) {
+                    if (!is_int($arg)) {
                         throw new IllegalArgumentException('int expected');
                     }
                     break;
 
                 case 'r':
-                    if (!is_resource($args[$i])) {
+                    if (!is_resource($arg)) {
                         throw new IllegalArgumentException('resource expected');
                     }
                     break;
 
                 case 's':
-                    if (!is_string($args[$i])) {
+                    if (!is_string($arg)) {
                         throw new IllegalArgumentException('string expected');
                     }
                     break;
@@ -62,6 +64,8 @@ trait TypeSafetyTrait
                     throw new IllegalArgumentException('Unknown type at offset ' . $i . '. One of [bfirs-] expected');
                     break;
             }
+
+            $i++;
         }
     }
 }
